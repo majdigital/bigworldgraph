fa0ad489d617ebf44cf069ecd1b4406d62a5c014
Kaleidophon <dennis.ulmer@altechnologies.de>
2017-06-02 12:46:02 +0100

    Working commit.

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-clear && docker exec -it `docker ps --filter name=backend --format "{{.Names}}"` python3 ./french_wikipedia.py
+cd backend/data/stanford/
+docker build -t stanford .
+docker run -d -v `pwd`/models/:/stanford_models/ -p 6080:9000 --name stanford stanford
+
+docker exec -i -t `docker ps --filter name=stanford --format "{{.Names}}"` /bin/bash
+echo test >> /stanford_models/test.txt
+touch /stanford_models/test.txt
+
+#clear && docker exec -it `docker ps --filter name=backend --format "{{.Names}}"` python3 ./french_wikipedia.py
