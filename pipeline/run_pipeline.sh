822f1af1d8284db8df0469811dd388d0bd2fe484
Kaleidophon <dennis.ulmer@altechnologies.de>
2017-06-05 17:35:23 +0100

    Fixed volume for backend container.

--- pipeline/run_pipeline.sh
+++ pipeline/run_pipeline.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 echo Downloading stanford model files if necessary...
-ls -lah ./stanford/
 if [ ! -f ./stanford_models/UD_French.gz ]; then wget majconsulting.ch/newsletters/stanford_models_french_100417.zip -P ./stanford_models/; fi
 if [ -f ./stanford_models/stanford_models_french_100417.zip ]; then unzip ./stanford_models/stanford_models_french_100417.zip -d ./stanford_models/ && rm ./stanford_models/stanford_models_french_100417.zip; fi
 
@@ -17,10 +16,12 @@ fi
 # Build stanford container and run pipeline
 cd ./stanford/
 docker build -t stanford .
-docker run -d -v `pwd`/stanford/models/:/stanford_models/ -p 6080:9000 --host="local" --name stanford stanford
-docker ps
+docker run -d -v /stanford_models/:/stanford_models/ -p 6080:9000 --network="bigworldgraph_app-tier" --name stanford stanford
+docker network ls
+ls -lah
+ls -lah /stanford_models/
 echo stanford name: `docker ps --filter name=stanford --format "{{.Names}}"`
-docker network connect --alias stanford bigworldgraph_app-tier `docker ps --filter name=stanford --format "{{.Names}}"`
-#clear && docker exec `docker ps --filter name=backend --format "{{.Names}}"` python3 ./french_wikipedia.py
+#docker network connect --alias stanford bigworldgraph_app-tier `docker ps --filter name=stanford --format "{{.Names}}"`
+clear && docker exec `docker ps --filter name=backend --format "{{.Names}}"` python3 ./french_wikipedia.py
 #echo Stopping stanford container...
 #docker rm $(docker stop $(docker ps --filter name=stanford --format "{{.Names}}"))
